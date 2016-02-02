7b84bf550846f62b6bf8d2078f6843a7c9c6d508
Heehong Moon <bbirec@gmail.com>
2016-02-02 22:21:12 +0900

    Enable “asar” option for packaging. Otherwise the win32 exe can not find the nodejs module correctly.

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-electron-packager . KOEN --platform=darwin --arch=x64 --version=0.36.2  --overwrite=true --icon=icon/icon --out=out
+electron-packager . KOEN --platform=darwin --arch=x64 --version=0.36.2  --overwrite=true --icon=icon/icon --asar=true --out=out
+electron-packager . KOEN --platform=win32 --arch=ia32 --version=0.36.2  --overwrite=true --icon=icon/icon --asar=true --out=out
